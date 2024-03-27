aff53c5725ab239fdcf8eb5e5fbc8b0c35fc3d06
yzm <13884935@qq.com>
2024-03-27 13:46:28 +0900

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "RTFchian",
-  description: "RTFchian",
+  title: "Hooplite",
+  description: "Hooplitean",
 };
 
 export default function RootLayout({
