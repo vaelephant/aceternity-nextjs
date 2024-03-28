a7791f257543932a3380ebeccf87ba05ef71c3e0
yzm <13884935@qq.com>
2024-03-28 12:21:25 +0900

    3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,103 @@ import { HeroParallax } from "@/components/ui/hero-parallax";
 import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 
 
+
+const products = [
+  {
+    title: "Moonbeam",
+    link: "https://gomoonbeam.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
+  },
+  {
+    title: "Cursor",
+    link: "https://cursor.so",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/cursor.png",
+  },
+  {
+    title: "Rogue",
+    link: "https://userogue.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/rogue.png",
+  },
+ 
+  {
+    title: "Editorially",
+    link: "https://editorially.org",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/editorially.png",
+  },
+  {
+    title: "Editrix AI",
+    link: "https://editrix.ai",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/editrix.png",
+  },
+  {
+    title: "Pixel Perfect",
+    link: "https://app.pixelperfect.quest",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
+  },
+ 
+  {
+    title: "Algochurn",
+    link: "https://algochurn.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
+  },
+  {
+    title: "Aceternity UI",
+    link: "https://ui.aceternity.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
+  },
+  {
+    title: "Tailwind Master Kit",
+    link: "https://tailwindmasterkit.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+  },
+  {
+    title: "SmartBridge",
+    link: "https://smartbridgetech.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
+  },
+  {
+    title: "Renderwork Studio",
+    link: "https://renderwork.studio",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
+  },
+ 
+  {
+    title: "Creme Digital",
+    link: "https://cremedigital.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
+  },
+  {
+    title: "Golden Bells Academy",
+    link: "https://goldenbellsacademy.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
+  },
+  {
+    title: "Invoker Labs",
+    link: "https://invoker.lol",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/invoker.png",
+  },
+  {
+    title: "E Free Invoice",
+    link: "https://efreeinvoice.com",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
+  },
+];
+
 export default function GoogleGeminiEffectDemo() {
   const ref = React.useRef(null);
   const { scrollYProgress } = useScroll({
@@ -37,9 +134,9 @@ export default function GoogleGeminiEffectDemo() {
     </div>
 
     
-    {/* <div>
+    <div>
     <HeroParallax products={products} />
-    </div>  */}
+    </div> 
 
 
     
@@ -48,99 +145,3 @@ export default function GoogleGeminiEffectDemo() {
   );
 }
 
-
-// export const products = [
-//   {
-//     title: "Moonbeam",
-//     link: "https://gomoonbeam.com",
-//     thumbnail:
-//       "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
-//   },
-//   {
-//     title: "Cursor",
-//     link: "https://cursor.so",
-//     thumbnail:
-//       "https://aceternity.com/images/products/thumbnails/new/cursor.png",
-//   },
-//   {
-//     title: "Rogue",
-//     link: "https://userogue.com",
-//     thumbnail:
-//       "https://aceternity.com/images/products/thumbnails/new/rogue.png",
-//   },
- 
-//   {
-//     title: "Editorially",
-//     link: "https://editorially.org",
-//     thumbnail:
-//       "https://aceternity.com/images/products/thumbnails/new/editorially.png",
-//   },
-//   {
-//     title: "Editrix AI",
-//     link: "https://editrix.ai",
-//     thumbnail:
-//       "https://aceternity.com/images/products/thumbnails/new/editrix.png",
-//   },
-//   {
-//     title: "Pixel Perfect",
-//     link: "https://app.pixelperfect.quest",
-//     thumbnail:
-//       "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
-//   },
- 
-//   {
-//     title: "Algochurn",
-//     link: "https://algochurn.com",
-//     thumbnail:
-//       "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
-//   },
-//   {
-//     title: "Aceternity UI",
-//     link: "https://ui.aceternity.com",
-//     thumbnail:
-//       "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
-//   },
-//   {
-//     title: "Tailwind Master Kit",
-//     link: "https://tailwindmasterkit.com",
-//     thumbnail:
-//       "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
-//   },
-//   {
-//     title: "SmartBridge",
-//     link: "https://smartbridgetech.com",
-//     thumbnail:
-//       "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
-//   },
-//   {
-//     title: "Renderwork Studio",
-//     link: "https://renderwork.studio",
-//     thumbnail:
-//       "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
-//   },
- 
-//   {
-//     title: "Creme Digital",
-//     link: "https://cremedigital.com",
-//     thumbnail:
-//       "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
-//   },
-//   {
-//     title: "Golden Bells Academy",
-//     link: "https://goldenbellsacademy.com",
-//     thumbnail:
-//       "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
-//   },
-//   {
-//     title: "Invoker Labs",
-//     link: "https://invoker.lol",
-//     thumbnail:
-//       "https://aceternity.com/images/products/thumbnails/new/invoker.png",
-//   },
-//   {
-//     title: "E Free Invoice",
-//     link: "https://efreeinvoice.com",
-//     thumbnail:
-//       "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
-//   },
-// ];
