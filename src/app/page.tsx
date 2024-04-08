fbca69d1e38c5b44ef6be3835ab6ed5e8680d7a8
yzm <13884935@qq.com>
2024-04-08 16:36:12 +0900

    修改了内容

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,94 +18,111 @@ function classNames(...classes: string[]): string {
 const products = [
   {
     title: "Moonbeam",
-    link: "https://gomoonbeam.com",
+    // link: "https://gomoonbeam.co",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
   },
   {
     title: "Cursor",
-    link: "https://cursor.so",
+    // link: "https://cursor.so",
+    link: "",
+
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/cursor.png",
   },
   {
     title: "Rogue",
-    link: "https://userogue.com",
+    // link: "https://userogue.com",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/rogue.png",
   },
 
   {
     title: "Editorially",
-    link: "https://editorially.org",
+    // link: "https://editorially.org",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/editorially.png",
   },
   {
     title: "Editrix AI",
-    link: "https://editrix.ai",
+    // link: "https://editrix.ai",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/editrix.png",
   },
   {
     title: "Pixel Perfect",
-    link: "https://app.pixelperfect.quest",
+    // link: "https://app.pixelperfect.quest",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
   },
 
   {
     title: "Algochurn",
-    link: "https://algochurn.com",
+    // link: "https://algochurn.com",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
   },
   {
     title: "Aceternity UI",
-    link: "https://ui.aceternity.com",
+    // link: "https://ui.aceternity.com",
+    link: "",
+
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
   },
   {
     title: "Tailwind Master Kit",
-    link: "https://tailwindmasterkit.com",
+    // link: "https://tailwindmasterkit.com",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
   },
   {
     title: "SmartBridge",
-    link: "https://smartbridgetech.com",
+    // link: "https://smartbridgetech.com",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
   },
   {
     title: "Renderwork Studio",
-    link: "https://renderwork.studio",
+    // link: "https://renderwork.studio",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
   },
 
   {
     title: "Creme Digital",
-    link: "https://cremedigital.com",
+    // link: "https://cremedigital.com",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
   },
   {
     title: "Golden Bells Academy",
-    link: "https://goldenbellsacademy.com",
+    // link: "https://goldenbellsacademy.com",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
   },
   {
     title: "Invoker Labs",
-    link: "https://invoker.lol",
+    // link: "https://invoker.lol",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/invoker.png",
   },
   {
     title: "E Free Invoice",
-    link: "https://efreeinvoice.com",
+    // link: "https://efreeinvoice.com",
+    link: "",
     thumbnail:
       "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
   },
--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -105,12 +105,10 @@ export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-        The Ultimate <br /> development studio
+      Discovering The Value<br /> of Data with Artificial Intelligence
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
-        We build beautiful products with the latest technologies and frameworks.
-        We are a team of passionate developers and designers that love to build
-        amazing products.
+      We utilize artificial intelligence technology and big data to provide better healthcare services for humanity, Making people happier.
       </p>
     </div>
   );
